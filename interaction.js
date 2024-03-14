6a2981db7e08693f11dd8430c48228046c086f97
Marfourz <boukarimarfourz@gmail.com>
2024-03-14 14:45:55 +0100

    feat : init project

--- interaction.js
+++ interaction.js
@@ -2,11 +2,53 @@
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
+
 	// Définir ici les attributs de la 'classe'
+  this.initX = 0
+  this.initY = 0
+  this.finalX = 0
+  this.finalY = 0
+  this.clicked = false
 
 	// Developper les 3 fonctions gérant les événements
+  this.onClick =  function(event){
+    this.clicked = true
+    var pos = getMousePosition(canvas, event)
+    this.initX = pos.x
+    this.initY = pos.y
+
+    console.log("Click détecté : ")
+    console.log("Position de la souris : " , {
+      "x" : this.initX,
+      "y" : this.initY
+    } )
+
+  }.bind(this)
+
+  this.onMouseMove = function(event){
+    if(this.clicked){
+      this.clicked = true
+      var pos = getMousePosition(canvas, event)
+      this.finalX = pos.x
+      this.finalY = pos.y
+      console.log("Position de la souris : " , {
+        "x" : this.finalX,
+        "y" : this.finalY
+      } )
+    }
+    
+  }.bind(this)
+
+  this.onMouseRelease = function(event){
+    this.clicked = false
+    console.log("Souris relachée : ", event)
+  }.bind(this)
 
 	// Associer les fonctions précédentes aux évènements du canvas.
+  canvas.addEventListener("mousedown", this.onClick)
+  canvas.addEventListener("mouseup", this.onMouseRelease)
+  canvas.addEventListener("mousemove", this.onMouseMove)
+
 };
 
 
--- model.js
+++ model.js
@@ -1,3 +1,29 @@
-
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+function Drawing(){
+    this.shapeArray = new Map()
+}
+
+
+function Shape(startX, startY, thickness, color){
+    this.color = color
+    this.thickness = thickness
+    this.startX = startX
+    this.startY = startY
+}
+
+
+function Line(startX, startY, thickness, color, startX, startY){
+    Line.call(this,startX, startY, thickness, color)
+    this.startX = startX
+    this.startY = startY
+}
+
+
+function Rectangle(startX, startY, thickness, color, height, width){
+    Line.call(this,startX, startY, thickness, color)
+    this.height = height
+    this.width = width
+    
+}
